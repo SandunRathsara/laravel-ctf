4744704d0464a0a65f296a775bb18cf9b55df4b2
Sandun Rathsara <srathsara1@gmail.com>
2020-09-21 21:59:12 +0530

    added authentication

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,10 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/home');
+Auth::routes();
 
-Route::get('/home', function () {
-    return view('home');
-});
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
