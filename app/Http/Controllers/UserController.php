fa48201ce2fe07e67a084aa33947a05ce4035102
Sandun Rathsara <srathsara1@gmail.com>
2020-09-22 23:26:40 +0530

    created first two levels

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+
+class UserController extends Controller
+{
+    public function index() {
+        return User::all();
+    }
+}
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,6 +15,7 @@
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-6">
+                                <!-- Ask and you shall receive -->
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
