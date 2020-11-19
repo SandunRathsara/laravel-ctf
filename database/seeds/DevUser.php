9b89d6a2fd95c01a3fe968a1384c08078c4cd061
Sandun Rathsara <srathsara1@gmail.com>
2020-11-19 09:10:32 +0530

    added devops user

--- database/seeds/DevUser.php
+++ database/seeds/DevUser.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class DevUser extends Seeder
 {
@@ -11,10 +14,14 @@ class DevUser extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
-            'name' => 'developer',
+        $user = User::create([
+            'name' => 'Developer',
             'email' => 'devops@gmail.com',
-            'password' => Hash::make('r4ZdEQ9c3cnqxvKM'),
+            'password' => bcrypt('r4ZdEQ9c3cnqxvKM')
         ]);
+        $role = Role::create(['name' => 'User']);
+        $permissions = Permission::pluck('id','id')->all();
+        $role->syncPermissions($permissions);
+        $user->assignRole([$role->id]);
     }
 }
